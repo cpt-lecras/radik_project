01b02c10c07dd74e0c8c260a9270a4189765c313
Dmitriy <tambovskiy_dima@mail.ru>
2023-03-04 20:30:01 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Распсиание'),
+        title: Text('Распсианиеttttttt'),
       ),
       body: Container(
         child: Center(
