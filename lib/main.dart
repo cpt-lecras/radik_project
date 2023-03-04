e90f6b7b6fb6e8624e2e4029c29a96611b1e4c80
Dmitriy <tambovskiy_dima@mail.ru>
2023-03-04 20:23:08 +0300

    AppBar HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp( MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      home: HomePage(),
+    );
+  }
 }
 
-class MyApp {
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Распсиание'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text('Лёха'),
+        ),
+      ),
+    );
+  }
 }
-
