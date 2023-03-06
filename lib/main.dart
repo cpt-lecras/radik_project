188fba03b86ce2d0b6f201cab07e178375cf1362
Dmitriy <tambovskiy_dima@mail.ru>
2023-03-06 21:00:56 +0300

    add BottomNavigator(работает)

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/material/bottom_navigation_bar.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.amber),
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black54,
+      backgroundColor: Colors.black38,
       appBar: AppBar(
         title: const Text('RGRTU-assistent'),
       ),
-      body: Container(
-        child: Center(
-          child: const Text('[место для расписания]', style: TextStyle(color: Colors.white),),
-        )
+      body: const Center(
+        child: Text('[место для расписания]', style: TextStyle(color: Colors.white),),
       ),
-      bottomNavigationBar: BottomNavigator(),
+      bottomNavigationBar: const BottomNavigator(),
     );
   }
 }
 
 class BottomNavigator extends StatelessWidget{
+  const BottomNavigator({super.key});
+
   @override
   Widget build(BuildContext context){
-    return BottomNavigationBar(items: const [
+    return BottomNavigationBar(
+        selectedIconTheme: const IconThemeData(color: Colors.black),
+        unselectedIconTheme: const IconThemeData(color: Colors.black),
+        backgroundColor: Colors.amberAccent,
+        items: const <BottomNavigationBarItem> [
       BottomNavigationBarItem(
-        icon: Icon(Icons.home, semanticLabel: "Home"),
+        icon: Icon(Icons.home),
+        label: 'Home',
       ),
       BottomNavigationBarItem(
-        icon: Icon(Icons.search, semanticLabel: "Search"),
-      ),
-    ]);
+        icon: Icon(Icons.search),
+        label: 'Search',
+      )
+    ],
+    );
   }
 }
+
