f2bcbfd9714ebf3b03787b7a53729473acaa52e2
LeCras <chetorkin.leha@gmail.com>
2023-03-10 14:52:32 +0300

    New BottomBar Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'map.dart';
 import 'news.dart';
 import 'profile.dart';
@@ -49,35 +50,42 @@ class _HomePage extends State<HomePage> {
       body: Center(
         child: widgetOptions.elementAt(selectedIndex),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: selectedIndex,
-        onTap: onItemTapped,
-        unselectedItemColor: Colors.amberAccent,
-        selectedItemColor: Colors.black,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.newspaper, color: Colors.black),
-            label: 'News',
+      bottomNavigationBar:Container(
+        color: Colors.amberAccent,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0,vertical: 10.0),
+          child: GNav(
             backgroundColor: Colors.amberAccent,
+            iconSize: 28.0,
+            color: Colors.black,
+            activeColor: Colors.amberAccent,
+            tabBackgroundColor:Colors.black ,
+            padding: EdgeInsets.all(10),
+            gap: 12,
+            onTabChange: (selectedIndex){
+              onItemTapped(selectedIndex);
+            },
+            tabs: const [
+              GButton(
+                  icon: Icons.newspaper,
+                  text: 'News',
+              ),
+              GButton(
+                  icon: Icons.today_outlined,
+                  text:'Schedule',
+              ),
+              GButton(
+                  icon: Icons.map,
+                  text:'Map',
+              ),
+              GButton(
+                  icon: Icons.person,
+                  text:'Account',
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.today_outlined, color: Colors.black),
-            label: 'Schedule',
-            backgroundColor: Colors.amberAccent,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.map, color: Colors.black),
-            label: 'Map',
-            backgroundColor: Colors.amberAccent,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person, color: Colors.black),
-            label: 'Account',
-            backgroundColor: Colors.amberAccent,
-          ),
-        ],
-
-      ),
+        ),
+      )
     );
   }
 }
