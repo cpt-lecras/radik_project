a268ec5cbb26b3ba22c4219e4af0d84195ba8928
Dmitriy <tambovskiy_dima@mail.ru>
2023-03-14 15:47:54 +0300

    assets

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class Account extends StatefulWidget{
   const Account({super.key});
@@ -10,11 +11,37 @@ class Account extends StatefulWidget{
 class _Account extends State<Account>{
   @override
   Widget build(BuildContext context){
-    return const Center(
-      child: Text(
-          'Account',
-        style: TextStyle(color: Colors.orangeAccent) ,
-      ),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            height: 70,
+            decoration: BoxDecoration(
+              color: Colors.blueGrey,
+                borderRadius: BorderRadius.circular(30),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: 165.0),
+              child: GNav(
+                iconSize: 30.0,
+                color: Colors.black,
+                activeColor: Colors.white70,
+                tabBackgroundColor: Colors.black12,
+                tabBorderRadius: 15,
+                gap: 15,
+                padding: EdgeInsets.all(15),
+                tabs: [
+                  GButton(
+                    icon: Icons.person,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
 
   }
